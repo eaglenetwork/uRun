7c4776b541d7c55e0368089f98c2fb58876784b3
EagleNetwork <153021443+eaglenetwork@users.noreply.github.com>
2024-03-26 11:04:03 -0700

    doing stuff\

--- urun.js
+++ urun.js
@@ -3,19 +3,23 @@ var popupEnabled = true
 document.addEventListener("keydown", function (e) {
 	if (e.key == "~" && e.ctrlKey) {
 		if (popupEnabled) {
-		var t = window.open("", "_blank", "width=500,height=300");
-		var e = t.document.createElement("iframe");
-		(e.src = "//eaglenetwork.github.io/uRun/popup.html"),
-			(e.style.cssText = "width:100%; height:100%; border:none;"),
-			t.document.body.appendChild(e),
-			t.document.title = "uRun",
-			t.addEventListener("message", function (e) {
-				e.data.toString().startsWith("execute:") && (eval(e.data.toString().replace("execute:", "")), t.close());
-			});} else {
+			var t = window.open("", "_blank", "width=500,height=300");
+			var e = t.document.createElement("iframe");
+			(e.src = "//eaglenetwork.github.io/uRun/popup.html"),
+				(e.style.cssText = "width:100%; height:100%; border:none;"),
+				t.document.body.appendChild(e),
+				t.document.title = "uRun",
+				t.addEventListener("message", function (e) {
+					e.data.toString().startsWith("execute:") && (eval(e.data.toString().replace("execute:", "")), t.close());
+				});
+		}
+		else {
 			eval(window.prompt("Code to run:"))
-			}}
+		}
+	}
 	if (e.key == "\\" && e.ctrlKey) {
+		if (popupEnabled) {
 		popupEnabled = false
 		alert("Popup disabled: manual mode triggered.")
-	}
+	}}
 });
